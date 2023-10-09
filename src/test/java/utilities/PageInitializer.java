dcb523cfcb312409cc3d79305cd035b3cb157168
alexzhao1995 <134657450+alexzhao1995@users.noreply.github.com>
2023-10-09 00:06:24 -0400

    added 3rd test case

--- src/test/java/utilities/PageInitializer.java
+++ src/test/java/utilities/PageInitializer.java
@@ -5,6 +5,7 @@ import pages.DirectoryPage;
 import pages.LandingPage;
 import pages.LoginPage;
 import pages.MyInfoPage;
+import pages.MyInfo_ContactDetailsPage;
 import pages.MyInfo_JobPage;
 import pages.MyInfo_PersonalDetailsPage;
 import pages.MyInfo_ReportToPage;
@@ -17,8 +18,9 @@ public class PageInitializer extends BaseClass {
 	public static MyInfoPage myInfoP;
 	public static MyInfo_ReportToPage reportToP;
 	public static MyInfo_JobPage jobP;
-	public static DirectoryPage dirP;
 	public static MyInfo_PersonalDetailsPage personalDetailsP;
+	public static MyInfo_ContactDetailsPage contactP;
+	public static DirectoryPage dirP;
 	
 	public static void initialize() {
 		lp = new LoginPage();
@@ -27,8 +29,9 @@ public class PageInitializer extends BaseClass {
 		myInfoP = new MyInfoPage();
 		reportToP = new MyInfo_ReportToPage();
 		jobP = new MyInfo_JobPage();
-		dirP = new DirectoryPage();
 		personalDetailsP = new MyInfo_PersonalDetailsPage();
+		contactP = new MyInfo_ContactDetailsPage();
+		dirP = new DirectoryPage();
 
 	}
 
